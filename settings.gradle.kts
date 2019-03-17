2ae7d40d2f4ee3b9612b7c6ee3c7932a2bed4031
Kamil Dąbrowski <kamil@namekdev.net>
2019-03-17 14:31:02 +0100

    cleanup gradle code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,19 +2,12 @@ pluginManagement {
     repositories {
         jcenter()
         maven(url = "https://plugins.gradle.org/m2/")
-//        maven(url = "https://dl.bintray.com/salomonbrys/gradle-plugins")
     }
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id == "kotlin-multiplatform") {
                 useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
             }
-//            if (requested.id.id.startsWith("com.github.salomonbrys.gradle.kotlin.js.")) {
-//                useModule("com.github.salomonbrys.gradle.kotlin.js:kotlin-js-gradle-utils:1.2.0")
-//            }
-//            if (requested.id.id.startsWith("com.moowork.")) {
-//                useModule("com.moowork.gradle:gradle-node-plugin:1.2.0")
-//            }
         }
     }
 }
