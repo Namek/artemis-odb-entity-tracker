581be07d10c0da62260092a884759d85803c5524
Kamil Dąbrowski <kamil@namekdev.net>
2019-03-24 02:11:13 +0100

    remove debug line after we fixed the checkbox bug!
    The bug where checkbox was checked even if it wasn't or vice-versa. It was due to unproper update of checkbox "checked" property inside the snabbdom patching algorithm.

--- src/jsMain/kotlin/net/namekdev/entity_tracker/Main.kt
+++ src/jsMain/kotlin/net/namekdev/entity_tracker/Main.kt
@@ -422,7 +422,6 @@ class Main(container: HTMLElement) : IWorldUpdateInterfaceListener<CommonBitVect
                 row(attrs(height(px(22))),
                     dataTypeToIcon(DataType.Boolean, model.isTypePrimitive),
                     text("${model.name ?: ""} = ⅟"),
-                    text((value as Boolean?).toString()),
                     nullableCheckbox(value as Boolean?, !model.isTypePrimitive) {
                         onValueChanged(rootValue, path, model.dataType, it)
                     }
